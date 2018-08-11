99a908f7fba7656aa3b0fc2d913862e0ddaec951
Mikhail Petrov <mixeys2008@gmail.com>
2018-08-11 14:28:05 +0300

    added media

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,20 @@
 // scripts
 
+
+// Carousel
+
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.carousel');
+//     var instances = M.Carousel.init(elems, {
+//         // numVisible: 3
+//     });
+//     instances.set(3);
+//   });
+
+// Media
+
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.carousel');
-    var instances = M.Carousel.init(elems, {
-        // numVisible: 3
-    });
-    instances.set(3);
+    var elems = document.querySelectorAll('.materialboxed');
+    var instances = M.Materialbox.init(elems, {});
   });
