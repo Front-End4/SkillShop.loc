bbf3d0674ed955dd3aee8aa59270ce9ffe7223cb
Alexander Vasuluev <alexhateful@gmail.com>
2018-08-11 14:41:50 +0300

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
 // scripts
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.carousel');
     var instances = M.Carousel.init(elems, {
         // numVisible: 3
     });
     instances.set(3);
 
-  });
+});
 
-  $(document).ready(function() {
+$(document).ready(function () {
     $('input#input_text, textarea#textarea2').characterCounter();
-  });
+});
