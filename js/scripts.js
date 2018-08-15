19d6bb43358b03c42a041507e2f6076d2fe24b1d
alexhateful <alexhateful@gmail.com>
2018-08-15 15:57:33 +0300

    add login page

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,6 @@
-// scripts
-
-
-// Carousel
-
-
-// document.addEventListener('DOMContentLoaded', function() {
-//     var elems = document.querySelectorAll('.carousel');
-//     var instances = M.Carousel.init(elems, {
-//         // numVisible: 3
-//     });
-//     instances.set(3);
-//   });
-
-// Media
-
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.materialboxed');
     var instances = M.Materialbox.init(elems, {});
-  });
-
-  $(document).ready(function () {
-    $('textarea#textarea2').characterCounter();
-    $('.modal').modal();
+    $('textarea#description').characterCounter();
 });
+
